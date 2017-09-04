7e1d9bea5be70742dae05a68478312da122b0f12
michael <michael@stratodem.com>
2017-09-03 20:52:52 -0400

    Strip flow types from build

--- dist/graphs/scatter.js
+++ dist/graphs/scatter.js
@@ -29,9 +29,11 @@ var _class, _temp;
 
 var _react = require('react');
 
-var _react2 = _interopRequireDefault(_react);
+var React = _interopRequireWildcard(_react);
 
 var _core = require('../core');
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -46,9 +48,9 @@ var ScatterPlot = (_temp = _class = function (_React$PureComponent) {
   (0, _createClass3.default)(ScatterPlot, [{
     key: 'render',
     value: function render() {
-      return _react2.default.createElement(_core.SDPlot, (0, _extends3.default)({}, this.props, { chartType: _core.utils.webGLSupported() ? 'scattergl' : 'scatter' }));
+      return React.createElement(_core.SDPlot, (0, _extends3.default)({}, this.props, { chartType: _core.utils.webGLSupported() ? 'scattergl' : 'scatter' }));
     }
   }]);
   return ScatterPlot;
-}(_react2.default.PureComponent), _class.defaultProps = _core.SDPlot.defaultProps, _temp);
+}(React.PureComponent), _class.defaultProps = _core.SDPlot.defaultProps, _temp);
 exports.default = ScatterPlot;
